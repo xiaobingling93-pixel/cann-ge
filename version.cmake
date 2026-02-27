4abb9f5270a42787a07a8f9ee7151db8e301b971
jikai-tyler <jikai5@huawei.com>
2026-02-27 17:01:41 +0800

    【PR】: 编译时版本号检查的依赖修改
    Co-authored-by: jikai-tyler<jikai5@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !742 merge master into master
    
    【PR】: 编译时版本号检查的依赖修改
    
    Created-by: jikai-tyler
    Commit-by: jikai-tyler
    Merged-by: cann-robot
    Description: 
    
    # Pull Request
    
    ## 描述
    编译时版本号检查的依赖修改，ge-executor和ge-compiler不依赖bisheng-compiler。
    
    ## 变更类型
    请选择本次引入的变更类型：
    <!--  [x] 表示选中 -->
    - [x] 🐛 Bug 修复
    - [ ] ✨ 新功能
    - [ ] 💄 代码风格更新（格式化，局部变量）
    - [ ] ♻️ 重构（既不修复错误也不增加功能的代码变动）
    - [ ] 📦 构建过程或辅助工具的变动
    - [ ] 📝 文档内容更新
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在当前页面的右侧'关联Issue'部分添加相应Issue链接，并勾选'合并后关闭已关联的 Issue'选项。 -->
    
    
    ## 如何测试
    描述测试此变更的步骤和前提条件：
    1.执行相关编译任务
    
    ## 核对清单
    <!--  [x] 表示选中 -->
    - [x] 我的代码遵循了项目的代码风格
    - [x] 我已对代码进行了自测
    - [x] 我已更新了相关的文档
    - [x] 我在标题中使用了合适的类型标签（如：`feat:`, `fix:`）
    - [x] 我已经详细阅读了贡献指南（CONTRIBUTING.md），并遵守了其中的所有规定，包括但不限于commit message的格式、无效commit的合并等
    
    ## 其他信息
    在此添加任何其他关于本次 PR 的说明。
    
    
    
    
    See merge request: cann/ge!742

--- version.cmake
+++ version.cmake
@@ -13,29 +13,26 @@ set_package(ge-executor VERSION "9.0.0")
 set_build_dependencies(runtime ">=9.0")
 set_build_dependencies(metadef ">=9.0")
 set_build_dependencies(hcomm ">=9.0")
-set_build_dependencies(bisheng-compiler ">=9.0")
 set_build_dependencies(asc-devkit ">=9.0")
 
 set_run_dependencies(runtime "CUR_MAJOR_MINOR_VER")
 set_run_dependencies(metadef "CUR_MAJOR_MINOR_VER")
 set_run_dependencies(hcomm "CUR_MAJOR_MINOR_VER")
 set_run_dependencies(asc-devkit "CUR_MAJOR_MINOR_VER")
-set_run_dependencies(bisheng-compiler "CUR_MAJOR_MINOR_VER")
 
 set_package(ge-compiler VERSION "9.0.0")
 
 set_build_dependencies(runtime ">=9.0")
+set_build_dependencies(metadef ">=9.0")
 set_build_dependencies(hcomm ">=9.0")
 set_build_dependencies(asc-devkit ">=9.0")
-set_build_dependencies(bisheng-compiler ">=9.0")
 set_build_dependencies(ge-executor ">=9.0")
 
 set_run_dependencies(runtime "CUR_MAJOR_MINOR_VER")
 set_run_dependencies(metadef "CUR_MAJOR_MINOR_VER")
 set_run_dependencies(hcomm "CUR_MAJOR_MINOR_VER")
-set_run_dependencies(bisheng-compiler "CUR_MAJOR_MINOR_VER")
 set_run_dependencies(asc-devkit "CUR_MAJOR_MINOR_VER")
-
+set_run_dependencies(ge-executor "CUR_MAJOR_MINOR_VER")
 
 set_package(dflow-executor VERSION "9.0.0")
 
@@ -49,4 +46,4 @@ set_run_dependencies(runtime "CUR_MAJOR_MINOR_VER")
 set_run_dependencies(metadef "CUR_MAJOR_MINOR_VER")
 set_run_dependencies(hcomm "CUR_MAJOR_MINOR_VER")
 set_run_dependencies(bisheng-compiler "CUR_MAJOR_MINOR_VER")
-set_run_dependencies(asc-devkit "CUR_MAJOR_MINOR_VER")
+set_run_dependencies(ge-executor "CUR_MAJOR_MINOR_VER")
