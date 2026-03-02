37b6e0005119c799d8167b31eb65453c1697daa9
duhaowei <duhaowei@huawei.com>
2026-03-02 22:24:20 +0800

    fix: autofuse st split
    Co-authored-by: duhaowei<duhaowei@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !782 merge develop_af_st into develop
    
    fix: autofuse st split
    
    Created-by: duhaowei
    Commit-by: duhaowei
    Merged-by: cann-robot
    Description: 
    
    # Pull Request
    
    ## 描述
    自动融合ST用例拆分。
    
    ## 变更类型
    请选择本次引入的变更类型：
    <!--  [x] 表示选中 -->
    - [ ] 🐛 Bug 修复
    - [ ] ✨ 新功能
    - [ ] 💄 代码风格更新（格式化，局部变量）
    - [x] ♻️ 重构（既不修复错误也不增加功能的代码变动）
    - [ ] 📦 构建过程或辅助工具的变动
    - [ ] 📝 文档内容更新
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在当前页面的右侧'关联Issue'部分添加相应Issue链接，并勾选'合并后关闭已关联的 Issue'选项。 -->
    
    
    ## 如何测试
    描述测试此变更的步骤和前提条件：
    1.自动融合ST用例拆分，不涉及
    
    ## 核对清单
    <!--  [x] 表示选中 -->
    - [x] 我的代码遵循了项目的代码风格
    - [x] 我已对代码进行了自测
    - [ ] 我已更新了相关的文档
    - [x] 我在标题中使用了合适的类型标签（如：`feat:`, `fix:`）
    - [x] 我已经详细阅读了贡献指南（CONTRIBUTING.md），并遵守了其中的所有规定，包括但不限于commit message的格式、无效commit的合并等
    
    ## 其他信息
    在此添加任何其他关于本次 PR 的说明。
    
    
    
    
    See merge request: cann/ge!782

--- tests/run_test.sh
+++ tests/run_test.sh
@@ -764,15 +764,25 @@ main() {
 
   # module autofuse_framework
   if [ "X$ENABLE_GE_AUTOFUSE_FRAMEWORK" == "Xon" ]; then
+    # autofuse_framework ut
     if [ "X$ENABLE_UT" = "Xon" ]; then
       bash scripts/test/run_autofuse_test.sh -u -m framework -j $THREAD_NUM $VERBOSE $COVERAGE
+    fi
+    # autofuse_framework st
+    if [ "X$ENABLE_ST" = "Xon" ]; then
+      bash scripts/test/run_autofuse_test.sh -s -m framework -j $THREAD_NUM $VERBOSE $COVERAGE
     fi
   fi
 
   # module autofuse_ascendc_api
   if [ "X$ENABLE_GE_AUTOFUSE_ASCENDC_API" == "Xon" ]; then
+    # autofuse_ascendc_api ut
     if [ "X$ENABLE_UT" = "Xon" ]; then
       bash scripts/test/run_autofuse_test.sh -u -m ascendc_api -j $THREAD_NUM $VERBOSE $COVERAGE
+    fi
+    # autofuse_ascendc_api st
+    if [ "X$ENABLE_ST" = "Xon" ]; then
+      bash scripts/test/run_autofuse_test.sh -s -m ascendc_api -j $THREAD_NUM $VERBOSE $COVERAGE
     fi
   fi
 
